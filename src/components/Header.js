b88e282ebbb68b036e593b11cc2a128e48bf2118
Timbafun <rogerio.gomes.10@hotmail.com>
2025-10-13 14:58:01 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 export default function Header() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    // Verifica se existe usuário logado no localStorage
+    const user = localStorage.getItem("user");
+    setIsLogged(!!user);
+  }, []);
+
   return (
     <header className="header">
       <div className="logo">
         <Link to="/" className="logo-text">PoolMarket</Link>
       </div>
       <nav className="nav-links">
-        <Link to="/login" className="nav-button">Login</Link>
-        <Link to="/signup" className="nav-button">Cadastro</Link>
+        {isLogged ? (
+          <Link to="/user-area" className="nav-button">Área do Usuário</Link>
+        ) : (
+          <>
+            <Link to="/login" className="nav-button">Login</Link>
+            <Link to="/signup" className="nav-button">Cadastro</Link>
+          </>
+        )}
       </nav>
     </header>
   );
